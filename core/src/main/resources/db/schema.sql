bd13174c58bf9ac35d5b6c6ea812b70eb07789ee
froggypol <polinatsitovich@gmail.com>
2020-04-20 20:43:16 +0300

    Encrypted passwords

--- core/src/main/resources/db/schema.sql
+++ core/src/main/resources/db/schema.sql
@@ -82,7 +82,7 @@ create table orders
 CREATE  TABLE users
 (
 username VARCHAR(45) NOT NULL ,
-password VARCHAR(45) NOT NULL ,
+password VARCHAR(450) NOT NULL ,
 enabled TINYINT NOT NULL DEFAULT 1 ,
 PRIMARY KEY (username)
 );
